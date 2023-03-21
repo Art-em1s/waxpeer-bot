07518a303bb736cf8f7656242a9f28e1a6606742
Artemis <Artemis@w1z0.xyz>
2023-03-21 00:29:28 +0000

    fix: remove need for discord webhooks, leaving blank ignores option

--- src/notifications.ts
+++ src/notifications.ts
@@ -2,14 +2,16 @@ import axios from 'axios';
 import 'dotenv/config';
 
 export async function sendNotification(content: string) {
-    try {
-        await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-            content,
-        });
-    } catch (err) {
-        console.log('Error sending notification to Discord');
-        console.error(err);
+    if (process.env.DISCORD_WEBHOOK_URL) {
+        try {
+            await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+                content,
+            });
+        } catch (err) {
+            console.log('Error sending notification to Discord');
+            console.error(err);
+        }
     }
 
     console.log(content);
-};
+}
